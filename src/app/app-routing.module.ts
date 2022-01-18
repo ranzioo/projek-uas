5e4aa2e437e6591fa9a1e0a01e88919d682bb185
ricky <rickyadi503@gmail.com>
2022-01-18 15:42:28 +0700

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { DivisionComponent } from './division/division.component';
+import { EditDataComponent } from './edit-data/edit-data.component';
+import { HapusDataComponent } from './hapus-data/hapus-data.component';
+import { TambahDataComponent } from './tambah-data/tambah-data.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"dashboard",component:DashboardComponent},
+  {path:"divisi",component:DivisionComponent},
+  {path:"tambah-data",component:TambahDataComponent},
+  {path:"edit-data",component:EditDataComponent},
+  {path:"hapus-data",component:HapusDataComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppHeaderComponent } from './app-header/app-header.component';
+import { AppMenuComponent } from './app-menu/app-menu.component';
+import { AppFooterComponent } from './app-footer/app-footer.component';
+import { DivisionComponent } from './division/division.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TambahDataComponent } from './tambah-data/tambah-data.component';
+import { EditDataComponent } from './edit-data/edit-data.component';
+import { HapusDataComponent } from './hapus-data/hapus-data.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHeaderComponent,
+    AppMenuComponent,
+    AppFooterComponent,
+    DivisionComponent,
+    DashboardComponent,
+    TambahDataComponent,
+    EditDataComponent,
+    HapusDataComponent
   ],
   imports: [
     BrowserModule,
